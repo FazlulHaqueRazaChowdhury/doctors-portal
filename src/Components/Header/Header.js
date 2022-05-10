0ed6a5d9a320ed721512d88888c4547c8cf1329a
FazlulHaqueRazaChowdhury <fozlulcoc1592@gmail.com>
2022-05-10 19:29:48 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Header = () => {
+    const navData = [
+        { name: 'Home', path: '/' },
+        { name: 'About', path: '/' },
+        { name: 'Appointment', path: '/' },
+        { name: 'Reviews', path: '/' },
+        { name: 'Contact Us', path: '/' },
+        { name: 'Log In', path: '/' }
+    ]
+    return (
+        <div className='container mx-auto'>
+            <div class="navbar bg-base-100">
+                <div class="navbar-start">
+
+                    <a class="btn btn-ghost normal-case text-xl">Doctors Portal</a>
+                </div>
+                <div class="navbar-end dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {
+                            navData.map(nav => <li><Link to={nav.path}>{nav.name}</Link></li>)
+                        }
+                    </ul>
+                </div>
+                <div class="navbar-end hidden lg:flex">
+                    <ul class="menu menu-horizontal w-full p-0">
+                        {
+                            navData.map(nav => <li><Link className='whitespace-nowrap hover:bg-accent hover:text-white mx-2' to={nav.path}>{nav.name}</Link></li>)
+                        }
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
